b4619ebb38a57d2fa3a648c16a1367ed637765b0
Hideo Haga <sindobook@gmail.com>
2016-06-23 22:46:32 +0900

    select gamepad and display selected gamepad info.

--- static/js/gamepad.js
+++ static/js/gamepad.js
@@ -6,6 +6,106 @@
 
 var StrSelectGamepad = '<p>PCにゲームパッド(ジョイスティック)を接続して下さい。アナログスティックを操作するかボタンを押すと認識します。</p>'
 var StrGamepadInfo = '';
+var TargetPad = -1;
+
+var Map = {
+    'axes' : [],
+    'buttons' : [],
+    'ctrls_old' : {
+	turn: 0.0,
+	beam: 0.0,
+	backet: 0.0,
+	turn_backet : 0.0
+    },
+    'ctrls' : {
+	turn: 0.0,
+	beam: 0.0,
+	backet: 0.0,
+	turn_backet : 0.0
+    },
+    
+    'set_turn' : function(value) {
+	this.ctrls.turn = value;
+    },
+    
+    'set_beam' : function(value) {
+	this.ctrls.beam = value;
+    },
+    
+    'set_backet' : function(value) {
+	this.ctrls.backet = value;
+    },
+    
+    'set_trun_backet' : function(value) {
+	this.ctrl.trun_backet = value;
+    },
+    
+    'send' : function() {
+	this.ctrls_old.turn = this.ctrls.turn;
+	this.ctrls_old.beam = this.ctrls.beam;
+	this.ctrls_old.backet = this.ctrls.backet;
+	this.ctrls_old.turn_backete = this.ctrls.turn_backet;
+    },
+};
+
+$('#select-gamepad').on('click', '.list-group-item', function(e) {
+    TargetPad = this.id;
+});
+		   
+//code here
+function list_gamepad(pads) {
+    var last_id_list_html = $("#select-gamepad").html();
+    var id_list_html = StrSelectGamepad;
+    id_list_html = '';
+    for(var i=0; i<pads.length; i++){
+	if (pads[i]) {
+	    if (i == TargetPad) {
+		id_list_html += '<li id="'+i+'" class="list-group-item active">';
+	    } else {
+		id_list_html += '<li id="'+i+'" class="list-group-item">';
+	    }
+	    id_list_html += pads[i].id;
+	    id_list_html += '</li>';
+	}
+    }
+    if (id_list_html != last_id_list_html) {
+	$("#select-gamepad").html(id_list_html);
+    }
+}
+
+function info_gamepad(pad) {
+    var info_html = StrGamepadInfo;
+    if (pad) {
+	info_html += '<ul class="list-inline">';
+	for (i=0;i<pad.axes.length;i++) {
+	    info_html += '<li>';
+	    info_html += '<span class="label label-primary">AXIS '+i+'</span>';
+	    info_html += '<span class="label label-default">'+pad.axes[i].toFixed(5)+'</span>';
+	    info_html += '</li>';
+	}
+	info_html += '</ul>';
+	
+	info_html += '<ul class="list-inline">';
+	for(i=0;i<pad.buttons.length;i++) {
+	    var val = pad.buttons[i];
+	    var pressed = (val == 1.0);
+	    if (typeof(val) == "object") {
+		pressed = val.pressed;
+		val = val.value;
+	    }
+	    info_html += '<li>';
+	    info_html += '<span class="label label-primary">BUTTON '+i+'</span>';
+	    info_html += '<span class="label label-default">'+val+'</span>';
+	    info_html += '</li>';
+	    }
+	info_html += '</ul>';
+    }
+    var last_info_html = $("#gamepad-info").html();
+    if (info_html != last_info_html) {
+	$("#gamepad-info").html(info_html);
+    }
+}
+
 
 //Create AnimationFrame
 var rAF = window.mozRequestAnimationFrame ||
@@ -24,60 +124,10 @@ function scanGamepads() {
 	return;
     }
 
-    //gamepad id list
-    var last_id_list_html = $("#select-gamepad").html();
-    var id_list_html = StrSelectGamepad;
-    if (pads[0]) {
-	id_list_html = '<li class="list-group-item active">';
-	id_list_html += pads[0].id;
-	id_list_html += '</li>';
-    }
-    for(var i=1; i<pads.length; i++){
-	if (pads[i]) {
-	    id_list_html += '<li class="list-group-item">';
-	    id_list_html += pads[i].id;
-	    id_list_html += '</li>';
-	}
-    }
-    if (id_list_html != last_id_list_html) {
-	$("#select-gamepad").html(id_list_html);
-    }
-    
-    // id 0's info :-)
-    var target = 0;
-    var info_html = StrGamepadInfo;
-    if (pads[target]) {
-	info_html += '<ul class="list-inline">';
-	for (i=0;i<pads[target].axes.length;i++) {
-	    info_html += '<li>';
-	    info_html += '<span class="label label-primary">AXIS '+i+'</span>';
-	    info_html += '<span class="label label-default">'+pads[target].axes[i].toFixed(5)+'</span>';
-	    info_html += '</li>';
-	}
-	info_html += '</ul>';
-	
-	info_html += '<ul class="list-inline">';
-	for(i=0;i<pads[target].buttons.length;i++) {
-	    var val = pads[target].buttons[i];
-	    var pressed = (val == 1.0);
-	    if (typeof(val) == "object") {
-		pressed = val.pressed;
-		val = val.value;
-	    }
-	    info_html += '<li>';
-	    info_html += '<span class="label label-primary">BUTTON '+i+'</span>';
-	    info_html += '<span class="label label-default">'+val+'</span>';
-	    info_html += '</li>';
-	    }
-	info_html += '</ul>';
-    }
-    var last_info_html = $("#gamepad-info").html();
-    if (info_html != last_info_html) {
-	$("#gamepad-info").html(info_html);
-    }
+    list_gamepad(pads);
+    info_gamepad(pads[TargetPad]);
 
     // attach function
-
     
     rAF(scanGamepads);
 }
